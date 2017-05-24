12b0e542041b207a853656bb259af9c6b320586d
Marco Bolis <mbolis1984@gmail.com>
2017-05-24 10:09:16 +0200

    Encapsulation review:
    * added encapsulation and interfaces to file I/O

--- src/snowtaxy/Output.java
+++
@@ -1,7 +0,0 @@
-package snowtaxy;
-
-public class Output {
-	public String utenteToString(Utente utente) {
-		return utente.getNome() + ";" + utente.getCognome() + ";" + utente.getRuolo() + ";";
-	}
-}
